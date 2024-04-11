596912f5aeb84bd97fe70b323bf37d9f4276a2a7
Ruslan Lantsov <ruslanlantsov1997@gmail.com>
2024-04-11 20:22:50 +0300

    Getting Tasks

--- ToDoList/Core/Views/Home/ViewModel/HomeViewModel.swift
+++ ToDoList/Core/Views/Home/ViewModel/HomeViewModel.swift
@@ -15,18 +15,30 @@ class HomeViewModel: ObservableObject {
         getTasks()
     }
     
-    
     private func getTasks() {
-        let newTasks = [
-            TaskModel(uid: "1", taskId: "1", taskText: "Task 1", createdAt: .now, isCompleted: false),
-            TaskModel(uid: "2", taskId: "2", taskText: "Task 2", createdAt: .now, isCompleted: true),
-            TaskModel(uid: "3", taskId: "3", taskText: "Task 3", createdAt: .now, isCompleted: false),
-            TaskModel(uid: "4", taskId: "4", taskText: "Task 4", createdAt: .now, isCompleted: true),
-            TaskModel(uid: "5", taskId: "5", taskText: "Task 5", createdAt: .now, isCompleted: false),
-        ]
+        guard let currentUserID = auth.currentUser?.uid else { return }
         
-        tasks = newTasks
+        firestore.collection("tasks")
+            .whereField("uid", isEqualTo: currentUserID)
+            .order(by: "createdAt", descending: true)
+            .addSnapshotListener { snapshot, error in
+                guard let documents = snapshot?.documents else {
+                    print("Error fetching documents: \(error ?? NSError())")
+                    return
+                }
+                
+                self.tasks = documents.compactMap { snapshot in
+                    do {
+                        let task = try snapshot.data(as: TaskModel.self)
+                        return task
+                    } catch {
+                        print("Error decoding task: \(error)")
+                        return nil
+                    }
+                }
+        }
     }
+    
     
     @MainActor
     func addTask(taskText: String) async throws {
